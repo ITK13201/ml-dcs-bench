57238758b2a68b2b15999dec9d38a5568133d72c
Takumi Ikeda <tkik13201@gmail.com>
2024-10-13 18:13:38 +0900

    add: added prod bat file

---
+++ ml-dcs-bench/bin/prod.run.bat
@@ -0,0 +1,1 @@
+.\ml-dcs-bench.exe run --input-dir .\testcases --output-base-dir .\results --log-base-dir .\logs --sleep-time 20 --mtsa-jar-path .\mtsa-PCS_MachineLearning_v0.2.0.jar --memory-size 225 --mtsa-target TraditionalController
